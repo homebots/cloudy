bca9352cd39249867319ebaacfddb8bf860eaca3
Darlan Alves <me@darlanalv.es>
2019-08-11 20:18:34 +0200

    chore: remove docker base url

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-DOCKER_BASE="d.homebots.io/v2"
-
 images=`ls -1 images/`
+
 for image in $images; do
-  (cd images/$image && source .env && docker build -t $DOCKER_BASE/$DOCKER_IMAGE . && docker push $DOCKER_BASE/$DOCKER_IMAGE)
+  (cd images/$image && docker build -t homebots-$image .)
 done
