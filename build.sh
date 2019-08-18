9bdf0625d0d9b381b2abec6673d171ccf6793a37
Darlan Alves <me@darlanalv.es>
2019-08-18 20:57:06 +0200

    feat(*): add alfred to images

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 images=`ls -1 images/`
 
 for image in $images; do
-  (cd images/$image && docker build -t homebots-$image .)
+  (cd images/$image && (docker rmi $image || true) && docker build -t homebots-$image .)
 done
